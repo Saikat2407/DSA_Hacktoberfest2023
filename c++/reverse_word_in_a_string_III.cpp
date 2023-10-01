cc7f5b4dc68a18ed3a243b9563c20fe04de8ee8b
Subhamay Nath <63004195+Grey-Hat-07@users.noreply.github.com>
2023-10-01 22:50:36 +0530

    Create reverse_word_in_a_string_III.cpp

---
+++ c++/reverse_word_in_a_string_III.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int i=0,j=0;
+
+        for(i=0;i<s.size();i++){
+            if(s[i]==' '){
+                int k=i-1;
+                while(j<k){
+                    s[k]=s[k]+s[j];
+                    s[j]=s[k]-s[j];
+                    s[k]=s[k]-s[j];
+                    k--;
+                    j++;
+                }
+                j=i+1;
+                i++;
+            }
+        }
+         int k=i-1;
+                while(j<k){
+                    s[k]=s[k]+s[j];
+                    s[j]=s[k]-s[j];
+                    s[k]=s[k]-s[j];
+                    k--;
+                    j++;
+                }
+
+
+        return s;
+    }
+};
