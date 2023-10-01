7ace306d9d8108a148216bc814ffe99105f53ed6
Sukshan01 <101926757+Sukshan01@users.noreply.github.com>
2023-10-01 19:09:43 +0530

    Added code for 3Sum Problem

---
+++ 3Sum.java
@@ -0,0 +1,52 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        for(int i=0; i<n; i++){
+
+            if(i > 0 && nums[i] == nums[i-1])
+            continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while(j < k ){
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum < 0){
+                    j++;
+                }
+
+                else if(sum > 0){
+                    k--;
+                }
+
+                else{
+                    List<Integer> list = new ArrayList<>();
+
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+                    ans.add(list);
+                    j++;
+                    k--;
+
+                    while(j < k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+                    while(j < k && nums[k] == nums[k+1]){
+                        k--;
+                    }
+                }
+            }
+        }
+
+        return ans;
+        
+    }
+}
