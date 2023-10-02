e85b641d432e5aff150ab20b614ee1e7491b12fd
Abel Johnson <abeljohnson207@gmail.com>
2023-10-02 17:34:47 +0530

    Update 134_Gas_Station.cpp

--- c++/134_Gas_Station.cpp
+++ c++/134_Gas_Station.cpp
@@ -1,4 +1,11 @@
 /*
+LeetCode 134 GAS STATION
+Description : There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. 
+You begin the journey with an empty tank at one of the gas stations.
+Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, 
+otherwise return -1. If there exists a solution, it is guaranteed to be unique
+
 Question Link : https://leetcode.com/problems/gas-station/description/
 subission Link : https://leetcode.com/problems/gas-station/submissions/1010179879/
 
