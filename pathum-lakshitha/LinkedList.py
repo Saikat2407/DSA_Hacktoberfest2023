77bbd0b220564ff728be8013f48c34eab5a26104
Pathum Lakshitha <96337912+PathumLakshitha@users.noreply.github.com>
2023-10-03 00:20:49 +0530

    Create LinkedList.py

---
+++ pathum-lakshitha/LinkedList.py
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class Main {
+  public static void main(String[] args) {
+    LinkedList<String> cars = new LinkedList<String>();
+    cars.add("Audi");
+    cars.add("TOYOTA");
+    cars.add("BMW");
+    cars.add("Tesla");
+    cars.add("Mazda");
+    System.out.println(cars);
+  }
+}
