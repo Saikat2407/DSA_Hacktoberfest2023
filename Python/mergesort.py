eeb17852cffdfe2e34ec06245cf39c3d7da43531
Hrithik Purwar <72293452+hrithikpurwar@users.noreply.github.com>
2023-10-04 14:07:41 +0530

    Added Merge Sort in Python

---
+++ Python/mergesort.py
@@ -0,0 +1,35 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+    return arr
+
+arr = [64, 25, 12, 22, 11]
+sorted_arr = merge_sort(arr)
+print("Sorted Array:", sorted_arr)
