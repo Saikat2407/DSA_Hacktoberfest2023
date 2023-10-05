2734067b553d001a0bb868916d0db04c025faff5
Kirti Gupta <79806185+KirtiGupta3101@users.noreply.github.com>
2023-10-05 22:58:57 +0530

    Add files via upload

---
+++ c++/Graph/Shortest Path Algorithms and Problems/Bellman Ford Algorithm.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+	/*  Function to implement Bellman Ford
+	*   edges: vector of vectors which represents the graph
+	*   S: source vertex to start traversing graph with
+	*   V: number of vertices
+	*/
+	vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
+		vector<int> dist(V, 1e8);
+		dist[S] = 0;
+		for (int i = 0; i < V - 1; i++) {
+			for (auto it : edges) {
+				int u = it[0];
+				int v = it[1];
+				int wt = it[2];
+				if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+					dist[v] = dist[u] + wt;
+				}
+			}
+		}
+		// Nth relaxation to check negative cycle
+		for (auto it : edges) {
+			int u = it[0];
+			int v = it[1];
+			int wt = it[2];
+			if (dist[u] != 1e8 && dist[u] + wt < dist[v]) {
+				return { -1};
+			}
+		}
+
+
+		return dist;
+	}
+};
+
+
+int main() {
+
+	int V = 6;
+	vector<vector<int>> edges(7, vector<int>(3));
+	edges[0] = {3, 2, 6};
+	edges[1] = {5, 3, 1};
+	edges[2] = {0, 1, 5};
+	edges[3] = {1, 5, -3};
+	edges[4] = {1, 2, -2};
+	edges[5] = {3, 4, -2};
+	edges[6] = {2, 4, 3};
+
+	int S = 0;
+	Solution obj;
+	vector<int> dist = obj.bellman_ford(V, edges, S);
+	for (auto d : dist) {
+		cout << d << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
+
+// Output: 0 5 3 3 1 2
---
+++ c++/Graph/Shortest Path Algorithms and Problems/Floyd Warshall Algorithm.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+	void shortest_distance(vector<vector<int>>&matrix) {
+		int n = matrix.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == -1) {
+					matrix[i][j] = 1e9;
+				}
+				if (i == j) matrix[i][j] = 0;
+			}
+		}
+
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					matrix[i][j] = min(matrix[i][j],
+					                   matrix[i][k] + matrix[k][j]);
+				}
+			}
+		}
+
+
+
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == 1e9) {
+					matrix[i][j] = -1;
+				}
+			}
+		}
+	}
+};
+
+
+int main() {
+
+	int V = 4;
+	vector<vector<int>> matrix(V, vector<int>(V, -1));
+	matrix[0][1] = 2;
+	matrix[1][0] = 1;
+	matrix[1][2] = 3;
+	matrix[3][0] = 3;
+	matrix[3][1] = 5;
+	matrix[3][2] = 4;
+
+	Solution obj;
+	obj.shortest_distance(matrix);
+
+	for (auto row : matrix) {
+		for (auto cell : row) {
+			cout << cell << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+
+// Output:
+
+// 0 2 5 -1
+// 1 0 3 -1
+// -1 -1 0 -1
+// 3 5 4 0
