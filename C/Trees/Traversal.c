8664ec38cebdb7c196180f75ac385547611ceffe
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2023-10-06 03:01:10 +0530

    Create Traversal.c

---
+++ C/Trees/Traversal.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int data) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+void inorderTraversal(struct TreeNode* root) {
+    if (root == NULL) {
+        return;
+    }
+    inorderTraversal(root->left);
+    printf("%d ", root->data);
+    inorderTraversal(root->right);
+}
+
+void preorderTraversal(struct TreeNode* root) {
+    if (root == NULL) {
+        return;
+    }
+    printf("%d ", root->data);
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+void postorderTraversal(struct TreeNode* root) {
+    if (root == NULL) {
+        return;
+    }
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    printf("%d ", root->data);
+}
+
+int main() {
+    struct TreeNode* root = NULL;
+    root = createNode(50);
+    root->left = createNode(30);
+    root->right = createNode(70);
+    root->left->left = createNode(20);
+    root->left->right = createNode(40);
+    root->right->left = createNode(60);
+    root->right->right = createNode(80);
+    
+    printf("In-order traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    printf("Pre-order traversal: ");
+    preorderTraversal(root);
+    printf("\n");
+
+    printf("Post-order traversal: ");
+    postorderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
