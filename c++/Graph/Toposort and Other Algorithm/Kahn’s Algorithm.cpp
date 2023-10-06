5f2dabc11d457ef8fa5f72675837359970cd3d09
Kirti Gupta <79806185+KirtiGupta3101@users.noreply.github.com>
2023-10-06 11:07:10 +0530

    Add files via upload

---
+++ c++/Graph/Toposort and Other Algorithm/Kahn’s Algorithm.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+	//Function to return list containing vertices in Topological order.
+	vector<int> topoSort(int V, vector<int> adj[])
+	{
+		int indegree[V] = {0};
+		for (int i = 0; i < V; i++) {
+			for (auto it : adj[i]) {
+				indegree[it]++;
+			}
+		}
+
+		queue<int> q;
+		for (int i = 0; i < V; i++) {
+			if (indegree[i] == 0) {
+				q.push(i);
+			}
+		}
+		vector<int> topo;
+		while (!q.empty()) {
+			int node = q.front();
+			q.pop();
+			topo.push_back(node);
+			// node is in your topo sort
+			// so please remove it from the indegree
+
+			for (auto it : adj[node]) {
+				indegree[it]--;
+				if (indegree[it] == 0) q.push(it);
+			}
+		}
+
+		return topo;
+	}
+};
+
+
+int main() {
+
+	//V = 6;
+	vector<int> adj[6] = {{}, {}, {3}, {1}, {0, 1}, {0, 2}};
+	int V = 6;
+	Solution obj;
+	vector<int> ans = obj.topoSort(V, adj);
+
+	for (auto node : ans) {
+		cout << node << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
+// Output: 4 5 0 2 3 1
