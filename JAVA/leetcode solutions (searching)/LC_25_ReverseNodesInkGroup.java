7b4d79ff594a67511da832977c5a8de5664d273f
Pranjal Goyal <100211916+dontwike@users.noreply.github.com>
2023-10-15 18:10:03 +0530

    Reviewed Changes

--- JAVA/leetcode solutions (searching)/LC_25_ReverseNodesInkGroup.java
+++ JAVA/leetcode solutions (searching)/LC_25_ReverseNodesInkGroup.java
@@ -1,6 +1,15 @@
-package LeetCode.LinkList;
+https://leetcode.com/problems/reverse-nodes-in-k-group/description/
 
-public class ReverseKGroup_25 extends LinkedList {
+public class ReverseKGroup_25{
+
+    public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
     public static void main(String[] args) {
 
     }
